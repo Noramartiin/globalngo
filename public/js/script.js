d7c535d592a96268c5ed6a7f4f5de17275c288c5
Alvaro Dalmau <dalmau.alvaro@gmail.com>
2021-02-19 12:09:21 +0100

    before presentation

--- public/js/script.js
+++ public/js/script.js
@@ -1,13 +1,24 @@
 document.addEventListener(
   'DOMContentLoaded',
   () => {
+    let menuLayout = document.querySelector('.menu-layout');
+    let menuDisplay = document.querySelectorAll('.menu-display');
     let editInformation = document.querySelector('.edit-profile');
     let formInformation = document.querySelector('.display-form-none');
     let deleteInformation = document.querySelector('.delete-profile');
     let divDelete = document.querySelector('.div-delete');
-    let editNgos = document.querySelector('.modify-ngos');
+    let editNgos = document.querySelector('.nav-profile');
     let divNgos = document.querySelector('.div-ngos');
 
+    menuLayout.addEventListener('click', () => {
+      menuDisplay.forEach(e => {
+        if (e.style.display == 'none') {
+          e.style.display = 'block';
+        } else {
+          e.style.display = 'none';
+        }
+      });
+    });
     editInformation.addEventListener('click', () => {
       if (formInformation.style.display == 'none') {
         formInformation.style.display = 'block';
